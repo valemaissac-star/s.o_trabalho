8aa71db6d87ee4fa988c515cc022b4f48b73daeb
Samuel Bandeira <valemais.sac@gmail.com>
2026-09-24 14:15:53 -0300

    questão 16 add

--- 16_questao.dart
+++ 16_questao.dart
@@ -8,8 +8,20 @@ void main() async {
   if (await arquivo1.exists() && await arquivo2.exists()) {
     String conteudoTexto1 = await arquivo1.readAsString();
     String conteudoTexto2 = await arquivo2.readAsString();
-    List<dynamic> listaJson1 = jsonDecode(conteudoTexto1) as List<dynamic>;
-    List<dynamic> listaJson2 = jsonDecode(conteudoTexto2) as List<dynamic>;
+    List<dynamic> usuarios = jsonDecode(conteudoTexto1) as List<dynamic>;
+    List<dynamic> processos = jsonDecode(conteudoTexto2) as List<dynamic>;
+
+    Map<int, String> loginPorId = {};
+
+    for (var usuario in usuarios) {
+      loginPorId[usuario["uid"]] = usuario["login"];
+    }
+
+    for (var processo in processos) {
+      int uid = processo["uid"];
+      String login = loginPorId[uid] ?? "usuario não está na lista";
+      print("processo ${processo["pid"]} iniciador por ${login} ");
+    }
   } else {
     print("Arquivo não existe");
   }
